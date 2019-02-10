efd138cb7619005a2569080dc8fdbbaed801b469
valeriosclip <valeriosclip3@gmail.com>
2019-02-10 10:39:44 +0000

    adding title section to slides

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    
     $('#slides').superslides({
        animation: 'fade',
        play: 5000,
        pagination: false
     });
+
+    var typed = new Typed(".typed", {
+        strings: ["Customer support Specialist.", "Web Developer.", "Traveller."], 
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
 });
