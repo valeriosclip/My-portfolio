940733ddd2f19a31444e911bf928d2f8b22b6ae1
valeriosclip <valeriosclip3@gmail.com>
2019-02-12 21:28:29 +0000

    stats section second part

--- js/script.js
+++ js/script.js
@@ -33,7 +33,9 @@ $(document).ready(function(){
 	    }
 	});
 
-    var skillsTopOffset = $(".skillsSection").offset().top;
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
     $(window).scroll(function() {
 
 		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -50,6 +52,16 @@ $(document).ready(function(){
 		        }
 		    });
 		}
-    });
+
+		if( !countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+				$(".counter").each(function(){
+					var element = $(this);
+					var endVal = parseInt(element.text());
+			
+					element.countup(endVal);
+				})
+				countUpFinished = true;
+		}
+	});
 
 });
