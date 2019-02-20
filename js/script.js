f1fee0267c20bf73bdd1be099c1afbd41189f45c
valeriosclip <valeriosclip3@gmail.com>
2019-02-20 20:22:18 +0000

    loader and fixing bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+$(window).on("load", function() {
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+});
+
 $(document).ready(function(){
     
     $('#slides').superslides({
@@ -65,15 +80,6 @@ $(document).ready(function(){
 	});
 
 	$("[data-fancybox]").fancybox();
-
-	$(".items").isotope({
-		filter: '*',
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue: false
-		}
-	});
 
 	$("#filters a").click(function() {
 		 $("#filters .current").removeClass("current");
